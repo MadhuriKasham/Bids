eae789b175c72d484b323c4af1761a76751131eb
Madhuri Kasham <70937030+MadhuriKasham@users.noreply.github.com>
2023-06-30 00:38:07 +0530

    Update items.py

--- GovBids/items.py
+++ GovBids/items.py
@@ -8,16 +8,13 @@ import scrapy
 
 class GovbidsItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
+    name = scrapy.Field()
     pass
 
-# def splitdate(value):
-#     new_value = str((value.split('T',1))[0])
-#     return new_value
+
 
 class BidItem(scrapy.Item):
     Bid= scrapy.Field()
-    # print(Bid)
     Bid_No = scrapy.Field()
     Items = scrapy.Field()
     Quantity = scrapy.Field()
